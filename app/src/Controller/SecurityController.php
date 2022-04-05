88e1b30923269ca0f01a682301e920b12bfbbdc1
ivan <>
2022-04-05 16:55:06 +0200

    access_control

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -14,13 +14,10 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
+         if ($this->getUser()) {
+             return $this->redirectToRoute('target_path');
+         }
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
@@ -31,6 +28,7 @@ class SecurityController extends AbstractController
      */
     public function logout(): void
     {
+        dd('aze');
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
